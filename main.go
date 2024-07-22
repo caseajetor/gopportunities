00a83d67571442768ddba921233041e387f28b3a
renan <renan_paganini2@hotmail.com>
2024-07-22 18:17:55 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/caseajetor/gopportunities/router"
+import (
+	"github.com/caseajetor/gopportunities/config"
+	"github.com/caseajetor/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
